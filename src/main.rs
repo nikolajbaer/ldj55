81fa7755efdb79199eaaed84556d46f8f2f8babc
Nikolaj Baer <nikolaj.baer@gmail.com>
2024-04-13 18:32:44 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,22 @@ use rand::Rng;
 use std::time::Duration;
 
 const MAX_MANA:f32 = 10.0;
+const ATTACK_MANA:f32 = 1.0;
+const ATTACK_STRENGTH:f32 = 5.0;
 const GOBLIN_ATTACK_TIME:f32 = 3.0;
 const MANA_REPLENISH_RATE:f32 = 1.0;
+const GOBLIN_ATTACK_DISTANCE:f32 = 2.0;
+const MAX_DEMON_SCARE:f32 = 5.0;
+const MAX_HEALTH:f32 = 30.0;
+const WIZARD_ATTACK_TIMER:f32 = 2.0;
 
 #[derive(Component)]
 struct Wizard {
     speed: f32,
     mana: f32,
     health: f32,
+    gameover: bool,
+    attack_timer: f32,
 }
 
 #[derive(Component)]
@@ -37,6 +45,9 @@ struct Demon {
 #[derive(Component)]
 struct ManaText;
 
+#[derive(Component)]
+struct HealthText;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -44,13 +55,16 @@ fn main() {
         // .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, (setup,setup_hud))
         .add_systems(Update, (
-            keyboard_input_system,
+            wizard_move,
+            wizard_attack,
             mouse_look,
             goblin_move,
             goblin_spawner,
+            goblin_life,
             demon_summoner,
             demon_lifespan,
-            wizard_mana_replenish,
+            wizard_vitals,
+            display_events,
         ))
         .run();
 }
@@ -82,6 +96,31 @@ fn setup_hud(
             ..default()
         }),
         ManaText,
+    ));
+    commands.spawn((
+        TextBundle::from_sections([
+            TextSection::new(
+                "Health: ",
+                TextStyle {
+                    font_size: 32.0,
+                    ..default()
+                },
+            ),
+            TextSection::new(
+                "",
+                TextStyle {
+                    font_size: 32.0,
+                    ..default()   
+                },
+            )
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(40.0),
+            left: Val::Px(5.0),
+            ..default()
+        }),
+        HealthText,
     ));
 }
 
@@ -125,9 +164,11 @@ fn setup(
     //     transform: Transform::from_rotation(Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2)).with_translation(Vec3::new(0.0,0.0,0.5)),
     //     ..default()
     // })
-        .insert(Wizard { speed: 6.0, mana: 10.0, health: 100.0 })
+        .insert(Wizard { speed: 6.0, mana: 10.0, health: MAX_HEALTH, gameover: false, attack_timer: WIZARD_ATTACK_TIMER})
         .insert(RigidBody::KinematicPositionBased)
         .insert(Collider::ball(0.5))
+        // .insert(ActiveEvents::COLLISION_EVENTS)
+        // .insert(ActiveCollisionTypes::default() | ActiveCollisionTypes::KINEMATIC_KINEMATIC)
         .insert(KinematicCharacterController::default());
 
     commands.insert_resource(SpawnGoblinsConfig {
@@ -179,7 +220,6 @@ fn goblin_spawner(
 }
 
 fn demon_summoner(
-    time: Res<Time>,
     mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&Transform,&mut Wizard)>,
@@ -218,29 +258,33 @@ fn demon_lifespan(
     }
 }
 
-fn wizard_mana_replenish(
+fn wizard_vitals(
     time: Res<Time>,
-    mut set: ParamSet<(
-        Query<(&mut Wizard), With<Wizard>>,
-        Query<&mut Text, With<ManaText>>,
-    )>,
+    mut wizard_query: Query<&mut Wizard, With<Wizard>>,
+    mut text_query: Query<&mut Text, With<ManaText>>,
 ) {
-    let mut wizard = set.p0().single_mut();
+    let mut wizard = wizard_query.single_mut();
     wizard.mana += MANA_REPLENISH_RATE * time.delta_seconds();
     if wizard.mana > MAX_MANA {
         wizard.mana = MAX_MANA;
     }
-    let mut text = set.p1().single_mut();
-    text.sections[1].value = format!("{wizard.mana:.0}");
+    if wizard.health <= 0.0 && !wizard.gameover {
+        println!("You died!");
+        wizard.gameover = true;
+    }
+    let value = wizard.mana;
+    let mut text = text_query.single_mut();
+    text.sections[1].value = format!("{value:.0}");
 }
 
-fn keyboard_input_system(
+fn wizard_move(
     time: Res<Time>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut KinematicCharacterController,&Transform, &Wizard)>,
+    mut query: Query<(&mut KinematicCharacterController,&Transform, &Wizard), With<Wizard>>,
 ) {
     let mut direction = Vec2::new(0.0,0.0);
     let (mut controller,transform,wizard) = query.single_mut();
+    if wizard.gameover { return; }
     if keyboard_input.pressed(KeyCode::KeyW) {
         direction.y -= 1.0;
     }
@@ -256,22 +300,103 @@ fn keyboard_input_system(
     controller.translation = Some(direction.normalize() * wizard.speed * time.delta_seconds());
 }
 
+fn wizard_attack(
+    time: Res<Time>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    mut query: Query<(&Transform, &mut Wizard), With<Wizard>>,
+    mut goblin_query: Query<&mut Goblin, With<Goblin>>,
+    rapier_context: Res<RapierContext>,
+) {
+    let (transform, mut wizard) = query.single_mut();
+    if wizard.attack_timer > 0.0 {
+        wizard.attack_timer -= time.delta_seconds();
+        return;
+    }
+    if wizard.mana < ATTACK_MANA {
+        return;
+    }
+    // Get in-range goblins to hit? 
+    if mouse_button_input.pressed(MouseButton::Left) {
+        println!("Wizard Attack!");
+        
+        // Select goblins to strike
+        let shape = Collider::ball(3.0);
+        let shape_pos = Vec2::new(transform.translation.x,transform.translation.y);
+        let shape_rot = 0.0;
+
+        // TODO move shape to front of wizard
+        let filter = QueryFilter::default();
+        rapier_context.intersections_with_shape(
+            shape_pos, shape_rot, &shape, filter, |entity| {
+            if let Ok(mut goblin) = goblin_query.get_mut(entity) {
+                goblin.health -= ATTACK_STRENGTH;
+            }
+            return true;
+        });
+
+        wizard.mana -= ATTACK_MANA;
+        wizard.attack_timer = WIZARD_ATTACK_TIMER;
+    }
+}
+
+fn goblin_life (
+    time: Res<Time>,
+    mut query: Query<(&mut Goblin,Entity), With<Goblin>>,
+    mut commands: Commands,
+) {
+    for (mut goblin, entity) in query.iter_mut() {
+        if goblin.health <= 0.0 {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
 fn goblin_move (
    time: Res<Time>,
-   mut set: ParamSet<(
-    Query<&Transform, With<Wizard>>,
-    Query<(&mut KinematicCharacterController, &mut Transform), With<Goblin>>,
-   )>
+   mut wizard_query: Query<(&Transform, &mut Wizard), (With<Wizard>, Without<Goblin>, Without<Demon>)>,
+   mut goblin_query: Query<(&mut KinematicCharacterController, &mut Transform, &mut Goblin), (With<Goblin>, Without<Wizard>, Without<Demon>)>,
+   demon_query: Query<&Transform, (With<Demon>, Without<Wizard>, Without<Goblin>)>,
 ) {
-    let wizard_position = set.p0().single().translation.clone();
-    for (mut controller, mut transform) in set.p1().iter_mut() {
+    let (wizard_transform, mut wizard) = wizard_query.single_mut();
+    let wizard_position = wizard_transform.translation;
+    if wizard.gameover { return; }
+
+    let demon_position = match demon_query.is_empty() {
+        true => None,
+        false => Some(demon_query.single().translation),
+    };
+
+    for (mut controller, mut transform, mut goblin) in goblin_query.iter_mut() {
         // creepy goblins, always looking at the wizard
-        let direction3d = wizard_position - transform.translation; 
-        let direction = Vec2::new(direction3d.x,direction3d.y);
+        let to_wizard = wizard_position - transform.translation; 
+       
+        // unless a demon is nearby! 
+        let direction = match (to_wizard,demon_position) {
+            (_,None) => Vec2::new(to_wizard.x,to_wizard.y),
+            (to_wizard,Some(demon_position)) => {
+                let to_demon = demon_position - transform.translation;
+                let demon_distance = to_demon.length();
+                if to_wizard.length() < demon_distance || demon_distance > MAX_DEMON_SCARE {
+                    Vec2::new(to_wizard.x,to_wizard.y)
+                } else {
+                    // run away!
+                    Vec2::new(-to_demon.x,-to_demon.y)
+                }
+            },
+        };
+
         let rotation = Quat::from_rotation_z(-direction.x.atan2(direction.y));
         transform.rotation = rotation;
         // // and maybe moving towards the wizard
         controller.translation = Some(direction.normalize() * 1.5 * time.delta_seconds());
+
+        if to_wizard.length() <= GOBLIN_ATTACK_DISTANCE && goblin.attack_timer <= 0.0 {
+            println!("Attacking!");
+            wizard.health -= 1.0;
+            goblin.attack_timer = GOBLIN_ATTACK_TIME;
+        } else if goblin.attack_timer > 0.0 {
+            goblin.attack_timer -= time.delta_seconds();
+        }
     }
 }
 
@@ -297,5 +422,18 @@ fn mouse_look(
         let direction = (point - wizard.translation).normalize();
         let rotation = Quat::from_rotation_z(-direction.x.atan2(direction.y));
         wizard.rotation = rotation;
+    }
+}
+
+fn display_events(
+    mut collision_events: EventReader<CollisionEvent>,
+    mut contact_force_events: EventReader<ContactForceEvent>,
+) {
+    for collision_event in collision_events.read() {
+        println!("Received collision event: {:?}", collision_event);
+    }
+
+    for contact_force_event in contact_force_events.read() {
+        println!("Received contact force event: {:?}", contact_force_event);
     }
 }
